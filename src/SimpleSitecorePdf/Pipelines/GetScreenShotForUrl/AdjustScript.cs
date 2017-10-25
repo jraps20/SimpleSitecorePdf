ba1446b42cb02c05b7fa6ec63e0f8d00a1d69634
John Rappel <jrappel@geekhive.com>
2017-10-24 21:18:28 -0500

    Modified scripts to account for various use-cases.

--- src/SimpleSitecorePdf/Pipelines/GetScreenShotForUrl/AdjustScript.cs
+++ src/SimpleSitecorePdf/Pipelines/GetScreenShotForUrl/AdjustScript.cs
@@ -2,13 +2,15 @@
 
 namespace SimpleSitecorePdf.Pipelines.GetScreenShotForUrl
 {
-    public class AddTimeoutToScript : GenerateScreenShotProcessor
+    public class AdjustScript : GenerateScreenShotProcessor
     {
         public string Timeout { get; set; }
 
+        public bool DirectPdf { get; set; }
+
         private const string StandardExit = "phantom.exit();\r\n});";
 
-        private string TimeoutExit => "window.setTimeout(function () {\r\n    page.render('%outfile%');\r\n    phantom.exit();\r\n  }, " + Timeout + ");\r\n});";
+        private string TimeoutExit => "window.setTimeout(function () {\r\n    page.paperSize = {format: 'A4'};\r\n    page.settings.dpi = '96';\r\n    page.render('%outfile%', {format: '" + (DirectPdf ? "pdf" : "png") + "'});\r\n    phantom.exit();\r\n  }, " + Timeout + ");\r\n});";
 
         public override void Process(GetScreenShotForURLArgs args)
         {
@@ -19,6 +21,13 @@ namespace SimpleSitecorePdf.Pipelines.GetScreenShotForUrl
             // script cleanup
             args.Script = args.Script.Replace($"page.render('{outputFilePath}');", "");
             args.Script = args.Script.Replace("%outfile%", outputFilePath);
+
+            if (!DirectPdf)
+                return;
+
+            args.Script = args.Script.Replace(".png", ".pdf");
+            // adjust output file name for later consumption
+            args.OutputFilename = args.OutputFilename.Replace(".png", ".pdf");
         }
     }
 }
